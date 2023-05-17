6ce4b44fe5c09f0bfad8884817a99844b352f849
Ravi Shankar Jha <ravi.jha3369@gmail.com>
2023-05-17 17:19:16 +0530

    added app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import {useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios';
+function App() {
+  const [attempt, setAttempt]= useState(10);
+  const [evenDiv, setEvenDiv] = useState([]);
+  const [oddDiv, setOddDiv] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [play,setPlay] = useState(false);
+  const [show,setShow] = useState(false);
+  const [winner, setWinner] = useState('');
+  useEffect(()=>{
+    document.addEventListener('keydown',handler);
+    return ()=>{
+      document.removeEventListener('keydown',handler);
+    };
+  });
+
+  const handler = async (event) =>{
+    event.preventDefault();
+    if(event.key===' ' && play){
+      const response = await axios.get('https://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1');
+      const data = response.data[0];
+      if(data%2===0){
+        setEvenDiv([...evenDiv,data]);
+      }else{
+        setOddDiv([...oddDiv,data]);
+      }
+      setAttempt(prev=>prev-1);
+    }
+    if(event.key==='r' || event.key==='p'){
+      setVisible(true);
+      setPlay(true);
+      setEvenDiv([]);
+      setOddDiv([]);
+      setShow(false);
+      setAttempt(10);
+    }
+    else if(event.key==='s'){
+      setVisible(false);
+      setPlay(false);
+    }
+    if(attempt===1){
+      setPlay(false);
+      setVisible(false);
+      const oddData=oddDiv.reduce(((acc,el)=>acc+el),0);
+      const evenData=evenDiv.reduce(((acc,el)=>acc+el),0);
+      setShow(true);
+      if(evenData>oddData){
+        setWinner('even');
+      }else{
+        setWinner('odd');
+      }
+    }
+  }
+  return (
+    <div className="App">
+      <h1>Play Room</h1>
+      <div className={visible ?'visible':'invisible'}>
+        <p>The game has {attempt} attempts left</p>
+        <p>Hit space to play the next turn</p>
+      </div>
+      <section role="playground" className="playground">
+        <section className="left container">
+          <div className="scoreBoard">
+            <p>Player Even</p>
+            <p>Total Score {evenDiv.reduce(((acc,el)=>acc+el),0)}</p>
+          </div>
+          <div className="scores">
+            {evenDiv.map((evenData,index)=>(
+              <div className="data" key={index}>{evenData}</div>
+            ))}
+          </div>
+          {show ? winner==='even' && <h1>You Win</h1>:''}
+        </section>
+        <section className="right container">
+          <div className="scoreBoard">
+              <p>Player Odd</p>
+              <p>Total Score {oddDiv.reduce(((acc,el)=>acc+el),0)}</p>
+          </div>
+          <div className="scores">
+          {oddDiv.map((oddData,index)=>(
+              <div className="data" key={index}>{oddData}</div>
+            ))}
+          </div>
+          {show ? winner==='odd' && <h1>You Win</h1>:''}
+        </section>
+      </section>
+      <p className={visible?'invisible':'visible'}>Hit R to start a new game</p>
+      <p className={(visible && play)?'visible':'invisible'}> Hit S to stop the game</p>
+    </div>
+  )
+}
+
+export default App
